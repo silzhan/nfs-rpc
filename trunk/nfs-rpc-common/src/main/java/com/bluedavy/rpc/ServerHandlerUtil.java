377dd24c51f1ab453566de3c69760b1c1c616cc0
bluedavy@gmail.com <bluedavy@gmail.com@dc7441b6-5111-417d-fe6d-473877676b19>
2011-08-22 03:34:04 +0000

    Cache Server Processor Method Object

--- trunk/nfs-rpc-common/src/main/java/com/bluedavy/rpc/ServerHandlerUtil.java
+++ trunk/nfs-rpc-common/src/main/java/com/bluedavy/rpc/ServerHandlerUtil.java
@@ -8,10 +8,26 @@ import java.util.Map;
 
 public class ServerHandlerUtil {
 
+	// Server Processors     key: servicename    value: service instance
 	private static Map<String, Object> processors = new HashMap<String, Object>();
+	
+	// Cached Server Methods  key: instanceName#methodname$argtype_argtype
+	private static Map<String, Method> cacheMethods = new HashMap<String, Method>();
 	
 	public static void registerProcessor(String instanceName,Object instance){
 		processors.put(instanceName, instance);
+		Class<?> instanceClass = instance.getClass();
+		Method[] methods = instanceClass.getMethods();
+		for (Method method : methods) {
+			Class<?>[] argTypes = method.getParameterTypes();
+			StringBuilder methodKeyBuilder = new StringBuilder();
+			methodKeyBuilder.append(instanceName).append("#");
+			methodKeyBuilder.append(method.getName()).append("$");
+			for (Class<?> argClass : argTypes) {
+				methodKeyBuilder.append(argClass.getName()).append("_");
+			}
+			cacheMethods.put(methodKeyBuilder.toString(), method);
+		}
 	}
 	
 	public static ResponseWrapper handleRequest(final RequestWrapper request){
@@ -28,13 +44,16 @@ public class ServerHandlerUtil {
 				throw new Exception("no "+targetInstanceName+" instance exists on the server");
 			}
 			if (argTypes != null && argTypes.length > 0) {
+				StringBuilder methodKeyBuilder = new StringBuilder();
+				methodKeyBuilder.append(targetInstanceName).append("#");
+				methodKeyBuilder.append(methodName).append("$");
 				Class<?>[] argTypeClasses = new Class<?>[argTypes.length];
 				for (int i = 0; i < argTypes.length; i++) {
+					methodKeyBuilder.append(argTypes[i]).append("_");
 					argTypeClasses[i] = Class.forName(argTypes[i]);
 				}
 				requestObjects = new Object[argTypes.length];
-				method = processor.getClass().getMethod(methodName,
-						argTypeClasses);
+				method = cacheMethods.get(methodKeyBuilder.toString());
 				Object[] tmprequestObjects = request
 						.getRequestObjects();
 				for (int i = 0; i < tmprequestObjects.length; i++) {
