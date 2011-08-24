6513cb889354695962d14ee29f7b5208bd89549a
bluedavy@gmail.com <bluedavy@gmail.com@dc7441b6-5111-417d-fe6d-473877676b19>
2011-08-24 13:14:16 +0000

    change package to code.google.nfs.rpc

--- trunk/nfs-rpc-common/src/main/java/com/bluedavy/rpc/benchmark/AbstractRPCBenchmarkServer.java
+++
@@ -1,9 +0,0 @@
-package com.bluedavy.rpc.benchmark;
-
-public abstract class AbstractRPCBenchmarkServer extends AbstractBenchmarkServer{
-
-	public Object getServerProcessor(int responseSize) {
-		return new BenchmarkTestServiceImpl(responseSize);
-	}
-
-}
--- trunk/nfs-rpc-common/src/main/java/com/bluedavy/rpc/benchmark/BenchmarkTestService.java
+++
@@ -1,7 +0,0 @@
-package com.bluedavy.rpc.benchmark;
-
-public interface BenchmarkTestService {
-
-	public ResponseObject execute(RequestObject request);
-	
-}
