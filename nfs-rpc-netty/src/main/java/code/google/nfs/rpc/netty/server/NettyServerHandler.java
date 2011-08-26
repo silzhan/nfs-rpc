473251c8ec2299ff923b17e91ecb4a637167bf11
bluedavy@gmail.com <bluedavy@gmail.com@dc7441b6-5111-417d-fe6d-473877676b19>
2011-08-26 05:11:15 +0000

    server pipeline change.

--- nfs-rpc-netty/src/main/java/code/google/nfs/rpc/netty/server/NettyServerHandler.java
+++ nfs-rpc-netty/src/main/java/code/google/nfs/rpc/netty/server/NettyServerHandler.java
@@ -8,9 +8,7 @@ package code.google.nfs.rpc.netty.server;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.RejectedExecutionException;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 
 import org.apache.commons.logging.Log;
@@ -21,7 +19,6 @@ import org.jboss.netty.channel.ExceptionEvent;
 import org.jboss.netty.channel.MessageEvent;
 import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
 
-import code.google.nfs.rpc.NamedThreadFactory;
 import code.google.nfs.rpc.ProtocolFactory;
 import code.google.nfs.rpc.RequestWrapper;
 import code.google.nfs.rpc.ResponseWrapper;
@@ -34,14 +31,9 @@ public class NettyServerHandler extends SimpleChannelUpstreamHandler {
 
 	private static final Log LOGGER = LogFactory.getLog(NettyServerHandler.class);
 	
-	private static final ThreadFactory tf = new NamedThreadFactory("SERVER-RECEIVER");
-	
-	private ExecutorService receiveThreadPool;
-	
 	private ExecutorService threadpool;
 	
 	public NettyServerHandler(ExecutorService threadpool){
-		this.receiveThreadPool = Executors.newCachedThreadPool(tf);
 		this.threadpool = threadpool;
 	}
 	
@@ -61,46 +53,42 @@ public class NettyServerHandler extends SimpleChannelUpstreamHandler {
 		}
 		@SuppressWarnings("unchecked")
 		final List<RequestWrapper> requests = (List<RequestWrapper>) e.getMessage();
-		receiveThreadPool.execute(new Runnable() {
-			public void run() {
-				for (final RequestWrapper request : requests) {
-					try{
-						threadpool.execute(new Runnable() {
-							public void run() {
-								long beginTime = System.currentTimeMillis();
-								ResponseWrapper responseWrapper = ProtocolFactory.getServerHandler().handleRequest(request);
-								int consumeTime = Integer.parseInt(""+(System.currentTimeMillis() - beginTime));
-								// timeout,so not return
-								if(consumeTime >= request.getTimeout()){
-									LOGGER.warn("timeout,so give up send response to client,request id is:"
-											+ request.getId()
-											+ ",client is:"
-											+ ctx.getChannel().getRemoteAddress()+",consumetime is:"+consumeTime+",timeout is:"+request.getTimeout());
-									return;
-								}
-								ChannelFuture wf = ctx.getChannel().write(responseWrapper);
-								wf.awaitUninterruptibly(request.getTimeout());
-								if(!wf.isSuccess()){
-									LOGGER.error("server write response error,request id is: "+request.getId());
-								}
-							}
-						});
-					}
-					catch(RejectedExecutionException exception){
-						LOGGER.error("server threadpool full,threadpool maxsize is:"
-								+ ((ThreadPoolExecutor) threadpool).getMaximumPoolSize());
-						ResponseWrapper responseWrapper = new ResponseWrapper();
-						responseWrapper.setRequestId(request.getId());
-						responseWrapper.setException(new Exception("server threadpool full,maybe because server is slow or too many requests"));
+		for (final RequestWrapper request : requests) {
+			try{
+				threadpool.execute(new Runnable() {
+					public void run() {
+						long beginTime = System.currentTimeMillis();
+						ResponseWrapper responseWrapper = ProtocolFactory.getServerHandler().handleRequest(request);
+						int consumeTime = Integer.parseInt(""+(System.currentTimeMillis() - beginTime));
+						// timeout,so not return
+						if(consumeTime >= request.getTimeout()){
+							LOGGER.warn("timeout,so give up send response to client,request id is:"
+									+ request.getId()
+									+ ",client is:"
+									+ ctx.getChannel().getRemoteAddress()+",consumetime is:"+consumeTime+",timeout is:"+request.getTimeout());
+							return;
+						}
 						ChannelFuture wf = ctx.getChannel().write(responseWrapper);
 						wf.awaitUninterruptibly(request.getTimeout());
 						if(!wf.isSuccess()){
 							LOGGER.error("server write response error,request id is: "+request.getId());
 						}
 					}
+				});
+			}
+			catch(RejectedExecutionException exception){
+				LOGGER.error("server threadpool full,threadpool maxsize is:"
+						+ ((ThreadPoolExecutor) threadpool).getMaximumPoolSize());
+				ResponseWrapper responseWrapper = new ResponseWrapper();
+				responseWrapper.setRequestId(request.getId());
+				responseWrapper.setException(new Exception("server threadpool full,maybe because server is slow or too many requests"));
+				ChannelFuture wf = ctx.getChannel().write(responseWrapper);
+				wf.awaitUninterruptibly(request.getTimeout());
+				if(!wf.isSuccess()){
+					LOGGER.error("server write response error,request id is: "+request.getId());
 				}
 			}
-		});
+		}
 	}
 	
 }
