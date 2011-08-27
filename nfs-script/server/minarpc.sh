b258aad1786fbb4738181f9d0a5ea132600b3271
bluedavy@gmail.com <bluedavy@gmail.com@dc7441b6-5111-417d-fe6d-473877676b19>
2011-08-27 05:40:31 +0000

    support multi protocol corunning,so server don't need run different.

--- nfs-script/server/minarpc.sh
+++
@@ -1,1 +0,0 @@
-sh servercommon.sh "minalib" "code.google.nfs.rpc.mina.benchmark.MinaRPCBenchmarkServer" "server.log.mina.rpc"
--- nfs-script/server/minasimple.sh
+++
@@ -1,1 +0,0 @@
-sh servercommon.sh "minalib" "code.google.nfs.rpc.mina.benchmark.MinaSimpleBenchmarkServer" "server.log.mina.simple"
