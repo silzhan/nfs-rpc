7a218acba3b34bb71d93f7ef60c88411e78d9fa0
bluedavy@gmail.com <bluedavy@gmail.com@dc7441b6-5111-417d-fe6d-473877676b19>
2011-09-09 03:40:05 +0000

    intergrate grizzly.

---
+++ nfs-rpc-grizzly/src/main/java/code/google/nfs/rpc/grizzly/serialize/GrizzlyByteBufferWrapper.java
@@ -0,0 +1,84 @@
+package code.google.nfs.rpc.grizzly.serialize;
+/**
+ * nfs-rpc
+ *   Apache License
+ *   
+ *   http://code.google.com/p/nfs-rpc (c) 2011
+ */
+import org.glassfish.grizzly.Buffer;
+import org.glassfish.grizzly.filterchain.FilterChainContext;
+import org.glassfish.grizzly.memory.MemoryManager;
+
+import code.google.nfs.rpc.protocol.ByteBufferWrapper;
+/**
+ * Grizzly ByteBuffer Wrapper
+ * 
+ * @author <a href="mailto:bluedavy@gmail.com">bluedavy</a>
+ */
+public class GrizzlyByteBufferWrapper implements ByteBufferWrapper {
+
+	private Buffer buffer;
+	
+	private FilterChainContext ctx;
+	
+	public GrizzlyByteBufferWrapper(FilterChainContext ctx){
+		this.ctx = ctx;
+	}
+	
+	public GrizzlyByteBufferWrapper(Buffer buffer){
+		this.buffer = buffer;
+	}
+	
+	public ByteBufferWrapper get(int capacity) {
+		@SuppressWarnings("unchecked")
+		MemoryManager<Buffer> memoryManager = ctx.getConnection().getTransport().getMemoryManager();
+		buffer = memoryManager.allocate(capacity);
+		buffer.allowBufferDispose(true);
+		return this;
+	}
+	
+	public Buffer getBuffer(){
+		return buffer;
+	}
+
+	public byte readByte() {
+		return buffer.get();
+	}
+
+	public void readBytes(byte[] data) {
+		buffer.get(data);
+	}
+
+	public int readInt() {
+		return buffer.getInt();
+	}
+
+	public int readableBytes() {
+		return buffer.remaining();
+	}
+
+	public int readerIndex() {
+		return 0;
+	}
+
+	public void setReaderIndex(int readerIndex) {
+		// Don't need,so do nothing
+	}
+
+	public void writeByte(byte data) {
+		buffer.put(data);
+	}
+
+	public void writeByte(int index, byte data) {
+		buffer.put(index, data);
+	}
+
+	public void writeBytes(byte[] data) {
+		buffer.put(data);
+	}
+
+	public void writeInt(int data) {
+		buffer.putInt(data);
+	}
+
+}
---
+++ nfs-rpc-grizzly/src/main/java/code/google/nfs/rpc/grizzly/server/GrizzlyServer.java
@@ -0,0 +1,81 @@
+package code.google.nfs.rpc.grizzly.server;
+/**
+ * nfs-rpc
+ *   Apache License
+ *   
+ *   http://code.google.com/p/nfs-rpc (c) 2011
+ */
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.glassfish.grizzly.filterchain.FilterChainBuilder;
+import org.glassfish.grizzly.filterchain.TransportFilter;
+import org.glassfish.grizzly.nio.transport.TCPNIOTransport;
+import org.glassfish.grizzly.nio.transport.TCPNIOTransportBuilder;
+import org.glassfish.grizzly.strategies.WorkerThreadIOStrategy;
+import org.glassfish.grizzly.threadpool.ThreadPoolConfig;
+
+import code.google.nfs.rpc.ProtocolFactory;
+import code.google.nfs.rpc.grizzly.serialize.GrizzlyProtocolFilter;
+import code.google.nfs.rpc.server.Server;
+/**
+ * Grizzly Server
+ * 
+ * @author <a href="mailto:bluedavy@gmail.com">bluedavy</a>
+ */
+public class GrizzlyServer implements Server {
+
+	private static final Log LOGGER = LogFactory.getLog(GrizzlyServer.class);
+	
+	private TCPNIOTransport transport = null;
+	
+	public void start(int listenPort, ExecutorService threadpool) throws Exception {
+		FilterChainBuilder filterChainBuilder = FilterChainBuilder.stateless();
+		filterChainBuilder.add(new TransportFilter());
+		filterChainBuilder.add(new GrizzlyProtocolFilter());
+		filterChainBuilder.add(new GrizzlyServerHandler());
+		TCPNIOTransportBuilder builder = TCPNIOTransportBuilder.newInstance();
+		ThreadPoolConfig config = builder.getWorkerThreadPoolConfig();
+		ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor)threadpool;
+		config.setCorePoolSize(threadPoolExecutor.getCorePoolSize()).setMaxPoolSize(threadPoolExecutor.getMaximumPoolSize()).setPoolName("GRIZZLY-SERVER");
+		transport = builder.build();
+		transport.setIOStrategy(WorkerThreadIOStrategy.getInstance());
+		transport.setProcessor(filterChainBuilder.build());
+		transport.bind(listenPort);
+		final TCPNIOTransport t = transport;
+		new Thread(new Runnable() {
+			public void run() {
+				try {
+					t.start();
+				} 
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+				while(true){
+					try {
+						Thread.sleep(Long.MAX_VALUE);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}).start();
+		LOGGER.warn("server started,listen at: "+listenPort);
+	}
+	
+	public void stop() throws Exception {
+		if(transport != null){
+			transport.stop();
+			LOGGER.warn("server stoped!");
+		}
+	}
+	
+	public void registerProcessor(Integer protocolType, String serviceName, Object serviceInstance) {
+		ProtocolFactory.getServerHandler(protocolType).registerProcessor(serviceName, serviceInstance);
+	}
+
+}
