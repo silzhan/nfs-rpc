0e9c3f6f3fcabe5e92d6f6e0ae7588d0f217c268
bluedavy@gmail.com <bluedavy@gmail.com@dc7441b6-5111-417d-fe6d-473877676b19>
2011-09-15 03:15:33 +0000

    server pipeline.

--- trunk/nfs-rpc-mina/src/main/java/code/google/nfs/rpc/mina/server/MinaServerHandler.java
+++ trunk/nfs-rpc-mina/src/main/java/code/google/nfs/rpc/mina/server/MinaServerHandler.java
@@ -54,63 +54,90 @@ public class MinaServerHandler extends IoHandlerAdapter {
 			throw new Exception(
 					"receive message error,only support RequestWrapper || List");
 		}
-		if(message instanceof List){
-			@SuppressWarnings("unchecked")
-			List<RequestWrapper> requests = (List<RequestWrapper>) message;
-			for (final RequestWrapper request : requests) {
-				handleOneRequest(session, request);
-			}
-		}
-		else{
-			handleOneRequest(session, (RequestWrapper)message);
-		}
+		handleRequest(session,message);
 	}
 
-	private void handleOneRequest(final IoSession session, final RequestWrapper request) {
+	@SuppressWarnings("unchecked")
+	private void handleRequest(final IoSession session, final Object message) {
 		try {
-			threadpool.execute(new Runnable() {
-				public void run() {
-					long beginTime = System.currentTimeMillis();
-					ResponseWrapper responseWrapper = ProtocolFactory.getServerHandler(request.getProtocolType()).handleRequest(request);
-					int consumeTime = Integer.parseInt(""+ (System.currentTimeMillis() - beginTime));
-					// already timeout,so not return
-					if (consumeTime >= request.getTimeout()) {
-						LOGGER.warn("timeout,so give up send response to client,requestId is:"
-								+ request.getId()
-								+ ",client is:"
-								+ session.getRemoteAddress()+",consumetime is:"+consumeTime+",timeout is:"+request.getTimeout());
-						return;
-					}
-					WriteFuture wf = session.write(responseWrapper);
-					wf.addListener(new IoFutureListener() {
-						public void operationComplete(IoFuture future) {
-							WriteFuture writeFuture = (WriteFuture) future;
-							if (!writeFuture.isWritten()) {
-								LOGGER.error("server write response error,maybe sendbuffer full or session is closed,session connected status: "
-										+ session.isConnected());
-							}
-						}
-					});
-				}
-			});
+			threadpool.execute(new HandlerRunnable(session, message, threadpool));
 		} 
 		catch (RejectedExecutionException exception) {
 			LOGGER.error("server threadpool full,threadpool maxsize is:"
 					+ ((ThreadPoolExecutor) threadpool).getMaximumPoolSize());
-			ResponseWrapper responseWrapper = new ResponseWrapper(request.getId(),request.getCodecType(),request.getProtocolType());
-			responseWrapper
-					.setException(new Exception("server threadpool full,maybe because server is slow or too many requests"));
-			WriteFuture wf = session.write(responseWrapper);
-			wf.addListener(new IoFutureListener() {
-				public void operationComplete(IoFuture future) {
-					WriteFuture writeFuture = (WriteFuture) future;
-					if (!writeFuture.isWritten()) {
-						LOGGER.error("server write response error,maybe sendbuffer full or session is closed,session connected status: "
-								+ session.isConnected());
-					}
+			if(message instanceof List){
+				List<RequestWrapper> requests = (List<RequestWrapper>) message;
+				for (final RequestWrapper request : requests) {
+					sendErrorResponse(session, request);
 				}
-			});
+			}
+			else{
+				sendErrorResponse(session, (RequestWrapper) message);
+			}
 		}
+	}
+
+	private void sendErrorResponse(final IoSession session,final RequestWrapper request) {
+		ResponseWrapper responseWrapper = new ResponseWrapper(request.getId(),request.getCodecType(),request.getProtocolType());
+		responseWrapper
+				.setException(new Exception("server threadpool full,maybe because server is slow or too many requests"));
+		WriteFuture wf = session.write(responseWrapper);
+		wf.addListener(new IoFutureListener() {
+			public void operationComplete(IoFuture future) {
+				if(!((WriteFuture)future).isWritten()){
+					LOGGER.error("server write response error,request id is: "+request.getId());
+				}
+			}
+		});
+	}
+	
+	class HandlerRunnable implements Runnable{
+
+		private IoSession session;
+		
+		private Object message;
+		
+		private ExecutorService threadPool;
+		
+		public HandlerRunnable(IoSession session,Object message,ExecutorService threadPool){
+			this.session = session;
+			this.message = message;
+			this.threadPool = threadPool;
+		}
+		
+		@SuppressWarnings("rawtypes")
+		public void run() {
+			// pipeline
+			if(message instanceof List){
+				List messages = (List) message;
+				for (Object messageObject : messages) {
+					threadPool.execute(new HandlerRunnable(session, messageObject, threadPool));
+				}
+			}
+			else{
+				RequestWrapper request = (RequestWrapper)message;
+				long beginTime = System.currentTimeMillis();
+				ResponseWrapper responseWrapper = ProtocolFactory.getServerHandler(request.getProtocolType()).handleRequest(request);
+				final int id = request.getId();
+				// already timeout,so not return
+				if ((System.currentTimeMillis() - beginTime) >= request.getTimeout()) {
+					LOGGER.warn("timeout,so give up send response to client,requestId is:"
+							+ id
+							+ ",client is:"
+							+ session.getRemoteAddress()+",consumetime is:"+(System.currentTimeMillis() - beginTime)+",timeout is:"+request.getTimeout());
+					return;
+				}
+				WriteFuture wf = session.write(responseWrapper);
+				wf.addListener(new IoFutureListener() {
+					public void operationComplete(IoFuture future) {
+						if(!((WriteFuture)future).isWritten()){
+							LOGGER.error("server write response error,request id is: "+id);
+						}
+					}
+				});
+			}
+		}
+		
 	}
 
 }
