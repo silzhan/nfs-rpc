7f83812ea9a2f94af732ed6edbcb767f97c8473a
kvrico@gmail.com <kvrico@gmail.com@dc7441b6-5111-417d-fe6d-473877676b19>
2011-09-19 19:08:24 +0000

    + update grizzly transport

--- nfs-rpc-grizzly/src/main/java/code/google/nfs/rpc/grizzly/server/GrizzlyServerHandler.java
+++ nfs-rpc-grizzly/src/main/java/code/google/nfs/rpc/grizzly/server/GrizzlyServerHandler.java
@@ -1,4 +1,5 @@
 package code.google.nfs.rpc.grizzly.server;
+
 /**
  * nfs-rpc
  *   Apache License
@@ -10,7 +11,6 @@ import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.glassfish.grizzly.CompletionHandler;
 import org.glassfish.grizzly.WriteResult;
 import org.glassfish.grizzly.filterchain.BaseFilter;
 import org.glassfish.grizzly.filterchain.FilterChainContext;
@@ -19,6 +19,12 @@ import org.glassfish.grizzly.filterchain.NextAction;
 import code.google.nfs.rpc.ProtocolFactory;
 import code.google.nfs.rpc.RequestWrapper;
 import code.google.nfs.rpc.ResponseWrapper;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ThreadPoolExecutor;
+import org.glassfish.grizzly.Connection;
+import org.glassfish.grizzly.EmptyCompletionHandler;
+
 /**
  * Grizzly Server Handler
  * 
@@ -26,73 +32,97 @@ import code.google.nfs.rpc.ResponseWrapper;
  */
 public class GrizzlyServerHandler extends BaseFilter {
 
-	private static final Log LOGGER = LogFactory.getLog(GrizzlyServerHandler.class);
-	
-	public NextAction handleRead(FilterChainContext ctx) throws IOException {
-		Object message = ctx.getMessage();
-		if (!(message instanceof RequestWrapper) && !(message instanceof List)) {
-			LOGGER.error("receive message error,only support RequestWrapper || List");
-			throw new IOException("receive message error,only support RequestWrapper || List");
-		}
-		if(message instanceof List){
-			@SuppressWarnings("unchecked")
-			List<RequestWrapper> requests = (List<RequestWrapper>) message;
-			for (final RequestWrapper request : requests) {
-				handleOneRequest(ctx, request);
-			}
-		}
-		else{
-			handleOneRequest(ctx, (RequestWrapper)message);
-		}
-		ctx.flush(null);
-		return ctx.getStopAction();
-	}
-	
-	public NextAction handleClose(FilterChainContext ctx) throws IOException {
-		return super.handleClose(ctx);
-	}
-	
-	public void exceptionOccurred(FilterChainContext ctx, Throwable error) {
-		error.printStackTrace();
-		super.exceptionOccurred(ctx, error);
-	}
-	
-	@SuppressWarnings("rawtypes")
-	private void handleOneRequest(final FilterChainContext ctx, final RequestWrapper request) {
-		long beginTime = System.currentTimeMillis();
-		ResponseWrapper responseWrapper = ProtocolFactory.getServerHandler(request.getProtocolType()).handleRequest(request);
-		int consumeTime = Integer.parseInt(""+ (System.currentTimeMillis() - beginTime));
-		// already timeout,so not return
-		if (consumeTime >= request.getTimeout()) {
-			LOGGER.warn("timeout,so give up send response to client,requestId is:"
-					+ request.getId()
-					+ ",client is:"
-					+ ctx.getConnection().toString()+",consumetime is:"+consumeTime+",timeout is:"+request.getTimeout());
-			return;
-		}
-		try {
-			ctx.write(responseWrapper, new CompletionHandler<WriteResult>() {
-				
-				public void updated(WriteResult result) {
-					;
-				}
-				
-				public void failed(Throwable t) {
-					LOGGER.error("server write response error",t);
-				}
-				
-				public void completed(WriteResult result) {
-					// IGNORE
-				}
-				
-				public void cancelled() {
-					LOGGER.error("server write response cancelled");
-				}
-			});
-		} 
-		catch (IOException e) {
-			LOGGER.error("server write response error",e);
-		}
-	}
-	
+    private static final Log LOGGER = LogFactory.getLog(GrizzlyServerHandler.class);
+
+    private final ExecutorService threadPool;
+
+    public GrizzlyServerHandler(final ExecutorService threadPool) {
+        this.threadPool = threadPool;
+    }
+    
+    public NextAction handleRead(final FilterChainContext ctx) throws IOException {
+        final Object message = ctx.getMessage();
+        final Connection connection = ctx.getConnection();
+
+        try {
+            threadPool.execute(new HandlerRunnable(connection, message, threadPool));
+        } catch (RejectedExecutionException exception) {
+            LOGGER.error("server threadpool full,threadpool maxsize is:"
+                    + ((ThreadPoolExecutor) threadPool).getMaximumPoolSize());
+            if (message instanceof List) {
+                List<RequestWrapper> requests = (List<RequestWrapper>) message;
+                for (final RequestWrapper request : requests) {
+                    sendErrorResponse(connection, request);
+                }
+            } else {
+                sendErrorResponse(connection, (RequestWrapper) message);
+            }
+        }
+
+        return ctx.getStopAction();
+    }
+    
+    private void sendErrorResponse(final Connection connection, final RequestWrapper request) throws IOException {
+        ResponseWrapper responseWrapper = new ResponseWrapper(request.getId(), request.getCodecType(), request.getProtocolType());
+        responseWrapper.setException(new Exception("server threadpool full,maybe because server is slow or too many requests"));
+
+        connection.write(responseWrapper, new EmptyCompletionHandler<WriteResult>() {
+
+            @Override
+            public void failed(Throwable throwable) {
+                  LOGGER.error("server write response error,request id is: " + request.getId());
+            }
+            
+        }).markForRecycle(true);
+    }
+
+    class HandlerRunnable implements Runnable {
+
+        private final Connection connection;
+        private final Object message;
+        private final ExecutorService threadPool;
+
+        public HandlerRunnable(Connection connection,
+                Object message,
+                ExecutorService threadPool) {
+            this.connection = connection;
+            this.message = message;
+            this.threadPool = threadPool;
+        }
+
+        @SuppressWarnings("rawtypes")
+        public void run() {
+            // pipeline
+            if (message instanceof List) {
+                List messages = (List) message;
+                for (Object messageObject : messages) {
+                    threadPool.execute(new HandlerRunnable(connection, messageObject, threadPool));
+                }
+            } else {
+                RequestWrapper request = (RequestWrapper) message;
+                long beginTime = System.currentTimeMillis();
+                ResponseWrapper responseWrapper = ProtocolFactory.getServerHandler(request.getProtocolType()).handleRequest(request);
+                final int id = request.getId();
+                // already timeout,so not return
+                if ((System.currentTimeMillis() - beginTime) >= request.getTimeout()) {
+                    LOGGER.warn("timeout,so give up send response to client,requestId is:"
+                            + id
+                            + ",client is:"
+                            + connection.getPeerAddress() + ",consumetime is:" + (System.currentTimeMillis() - beginTime) + ",timeout is:" + request.getTimeout());
+                    return;
+                }
+
+                try {
+                    connection.write(responseWrapper, new EmptyCompletionHandler<WriteResult>() {
+                        @Override
+                        public void failed(Throwable throwable) {
+                            LOGGER.error("server write response error,request id is: " + id);
+                        }
+                    }).markForRecycle(true);
+                } catch (IOException ignored) {
+                }
+            }
+        }
+    }
+    
 }
